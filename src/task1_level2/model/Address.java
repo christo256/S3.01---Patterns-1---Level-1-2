706f95a3880b0d3ce3cc5f850d070842e659f44e
christo256 <ckingmaster2@gmail.com>
2025-11-03 14:05:33 +0100

    feat(model): add Address and PhoneNumber interfaces

---
+++ src/task1_level2/model/Address.java
@@ -0,0 +1,5 @@
+package task1_level2.model;
+
+public interface Address {
+    String getFullAddress();
+}
---
+++ src/task1_level2/model/PhoneNumber.java
@@ -0,0 +1,5 @@
+package task1_level2.model;
+
+public interface PhoneNumber {
+    String getFullNumber();
+}
