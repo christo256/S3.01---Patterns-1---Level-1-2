fa21b79f810f9bb5b3257296f2d19eb06dbb7250
christo256 <ckingmaster2@gmail.com>
2025-11-03 12:48:41 +0100

    feat(menu): add MenuPrinter for printing menu and messages

---
+++ src/MenuPrinter.java
@@ -0,0 +1,24 @@
+public class MenuPrinter {
+
+    public void printerHeader() {
+        System.out.println("=== Undo Command Exercise ===");
+    }
+
+    public void printMenu() {
+        System.out.println("\nChoose an option: ");
+        System.out.println("1. Add command");
+        System.out.println("2. Remove last command");
+        System.out.println("3. Show command history");
+        System.out.println("4. Exit");
+        System.out.print("Option: ");
+    }
+
+    public void printInvalidOption() {
+        System.out.println("Invalid option. Try again.");
+    }
+
+    public void printExitMessage() {
+        System.out.println("Exiting program...");
+
+    }
+}
